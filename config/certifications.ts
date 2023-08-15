aacf935631e94f36555645c73055d9c30481defd
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-08-15 16:59:04 +0900

    upload cka

--- config/certifications.ts
+++ config/certifications.ts
@@ -3,7 +3,7 @@ import { Certification } from "../model/certification";
 export const certifications: Certification[] = [
   {
     name: "Certified Kubernetes\nAdministrator\n(CKA)",
-    url: "",
-    imageSrc: "/images/certification/cka.png",//TODO:CKAの画像、URL
+    url: "https://www.credly.com/badges/ec4a76f2-4522-4322-8440-7841e51fbb40/public_url",
+    imageSrc: "/images/certification/cka.png",
   },
 ];
