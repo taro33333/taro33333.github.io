ac8b74d21bbca0e4785126f9eab0e74617352ac1
RyotaroSeto <RyotaroSeto@users.noreply.github.com>
2023-08-23 15:26:12 +0000

    deploy: 28371ecdfaa2871be37143f0da52e4a1429ca3d3

--- _next/static/P1IkW-Agb9qAt_eWLoe_B/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/94fcb773-085acd8a03616799.js","static/chunks/79-63f5733d57e58126.js","static/chunks/pages/index-86a71edc58e4f081.js"],"/_error":["static/chunks/pages/_error-06db57579683f60c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
