391ea390b9a6c0f8146790cb2653e37c62e3db28
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-08-29 00:15:57 +0900

    add ckad Certified

--- config/certifications.ts
+++ config/certifications.ts
@@ -5,5 +5,10 @@ export const certifications: Certification[] = [
     name: 'Certified Kubernetes\nAdministrator\n(CKA)',
     url: 'https://www.credly.com/badges/ec4a76f2-4522-4322-8440-7841e51fbb40/public_url',
     imageSrc: '/images/certification/cka.png'
+  },
+  {
+    name: 'Certified KubernetesApplication Developer\n(CKAD)',
+    url: 'https://www.credly.com/earner/earned/badge/71a7c8ec-440e-42e0-ad51-f250dee8a81d',
+    imageSrc: '/images/certification/ckad.png'
   }
 ]
